f2eb2cd7f622f7e24fbfb2acbe03a6fda335fd4e
Steven Flintham <sgf@lemma.co.uk>
2021-04-10 16:50:19 +0100

    Add OSWORD &43 test program

---
+++ test/test.asm
@@ -0,0 +1,21 @@
+macro testrom id
+	org &8000
+	clear &8000,&BFFF
+	.header
+		equb 0, 0, 0
+		jmp service_entry
+		equb &82
+		equb copyright - header
+		equb 0
+		equs "Test ROM ", 'A'+id, 0
+	.copyright
+		equs 0, "(C)", 0
+	.service_entry
+		rts
+endmacro
+
+testrom 0
+save "ROMA", &8000, P%
+testrom 1
+save "ROMB", &8000, P%
+putbasic "osword43-a.bas", "OSW43A"
