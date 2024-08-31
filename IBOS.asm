kgl2001/IntegraB-OS

commit 217b52e1051cfd14e49bf80e1c5ef62f6cfd0b03

Re-enable *APPEND and fix transient workspace clash

IBOS.asm
@@ -236,7 +236,7 @@ transientDateSFTODO2 = &AA ; SFTODO: prob just temp storage
 transientDateSFTODO1 = &AB ; SFTODO!? 2 bytes?
 
 IF IBOS_VERSION >= 127
-transientBin = &A8	;2 bits added by KL for IBOS1.27
+transientBin = &AE	;2 bits added by KL for IBOS1.27
 transientBCD = &AC	;2 bits added by KL for IBOS1.27
 ENDIF
 
@@ -3460,14 +3460,12 @@ ENDIF
 			
 ;*APPEND Command
 .append
-
-; KL 3/8/24: Temporarily dropped this code, to free up space for IBOS127 Memory Bank Calculations.
-IF IBOS_VERSION < 127
 {
 ; SFTODO: Express these as transientWorkspace + n, to document what area of memory they live in?
 LineLengthIncludingCr = &A9
 LineNumber = &AA
 OswordInputLineBlockCopy = &AB ; 5 bytes
+
     LDA #osfindOpenUpdate:JSR ParseFilenameAndOpen
     LDA #0:STA LineNumber
     PRVEN
@@ -3530,11 +3528,6 @@ OswordInputLineBlockCopy = &AB ; 5 bytes
 .^printSpace
     LDA #' ':JMP OSWRCH
 }
-ELSE
-    JMP OSNEWLPrvDisExitAndClaimServiceCall
-.printSpace
-    LDA #' ':JMP OSWRCH
-ENDIF
 
 ; *PRINT command
 ; Note that this sends the file to the printer, *unlike* the Master *PRINT command which is
@@ -6505,8 +6498,8 @@ CurrentBank = TransientZP + 2
 BankCopyrightOffset = TransientZP + 3
 IF IBOS_VERSION >= 127
 ; PrintADecimal uses transientBin and transientBCD so we must fit round those.
-RamPresenceCopyLow = TransientZP + 6
-RamPresenceCopyHigh = TransientZP + 7
+RamPresenceCopyLow = TransientZP + 0
+RamPresenceCopyHigh = TransientZP + 1
 ENDIF
 
 IF IBOS_VERSION < 127

commit b13965f7cff0e09cd7d2dd077c95664cc25d8e08

Fix *STATUS LANG output of second value
This was broken in commit 217b52e1051cfd14e49bf80e1c5ef62f6cfd0b03.

IBOS.asm
@@ -246,8 +246,8 @@ transientDateSFTODO2 = &AA ; SFTODO: prob just temp storage
 transientDateSFTODO1 = &AB ; SFTODO!? 2 bytes?
 
 IF IBOS_VERSION >= 127
-transientBin = &AE	;2 bits added by KL for IBOS1.27
-transientBCD = &AC	;2 bits added by KL for IBOS1.27
+transientBin = &AE	;2 bytes added by KL for IBOS1.27
+transientBCD = &AC	;2 bytes added by KL for IBOS1.27
 ENDIF
 
 FilingSystemWorkspace = &B0; IBOS repurposes this, which feels a bit risky but presumably works in practice
@@ -4274,7 +4274,12 @@ ENDIF
 IF IBOS_VERSION >= 126
 .ConfLang
 {
-Tmp = TransientZP + 7 ; ConfRefDynamicSyntaxGenerationForTransientCmdIdx uses +6
+    IF IBOS_VERSION < 127
+        Tmp = TransientZP + 7 ; ConfRefDynamicSyntaxGenerationForTransientCmdIdx uses +6
+    ELSE
+        ; TransientZP + {4,5,6,7} are used by PrintADecimalNoPad
+        Tmp = TransientZP + 3
+    ENDIF
 
     BCC ConfLangRead
     JSR ConvertIntegerDefaultDecimalChecked:AND #maxBank:STA Tmp:PHA