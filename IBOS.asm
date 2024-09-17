f40cc7552937b0aefb72a20666a59c209bbdf0d0
Steven Flintham <sgf@lemma.co.uk>
2024-09-17 23:03:47 +0100

    Tweak comments

--- IBOS.asm
+++ IBOS.asm
@@ -7116,12 +7116,12 @@ ENDIF
 .LA22E      BIT prvOswordBlockCopy                                                                  ;function
             BPL PrvDisexitScIndirect                                                                ;branch if read
             BVS PrvDisexitScIndirect                                                                ;branch if pseudo-address
-            ; TODO: This looks like an "unofficial" extension to OSWORD &43 where b0 means
+            ; TODO: This looks like an unofficial extension to OSWORD &43 where b0 means
             ; "*INSERT the bank automatically" and b1 means "*SRWP the bank automatically after
             ; loading", but I am not sure.
             ; SFTODONOW: Ken - would you be able to test these two extensions please? I suspect
             ; you can do so fairly easily using the osword43-a.bas tweaked to run *without* a
-            ; second processor (to avoid the bug) and adjuting the value poked into block%?0 to
+            ; second processor (to avoid the bug) and adjusting the value poked into block%?0 to
             ; set b0 and/or b1. Beforehand try write-enabling and/or unplugging the bank and
             ; see if these are automatically undone.
             LSR prvOswordBlockCopy                                                                  ;function
